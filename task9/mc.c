f0c58ccca43657b627f42cb08ddf20fffc66ef84
Major <n.boywalenko@yandex.ru>
2025-06-04 20:59:24 +0700

    Create func createWin

--- task9/mc.c
+++ task9/mc.c
@@ -19,6 +19,27 @@ void changeStatus(char *str)
   wprintw(statusWin, "status: %s", str);
   wrefresh(statusWin);
 }
+WINDOW* createTable(int pos)
+{
+
+    int sizeCol = COLS / 6;
+    WINDOW* win = newwin(LINES, COLS / 2, 0, pos);
+    WINDOW* nameWin = subwin(win, LINES, sizeCol, 0, pos + sizeCol * 0);
+    WINDOW* sizeWin = subwin(win, LINES, sizeCol, 0, pos + sizeCol * 1);
+    WINDOW* dateWin = subwin(win, LINES, sizeCol, 0, pos + sizeCol * 2);
+
+    box(win, 0, 0);
+    wborder(sizeWin, ACS_VLINE, ACS_VLINE, ACS_HLINE, ACS_HLINE, ACS_TTEE, ACS_TTEE, ACS_BTEE, ACS_BTEE);
+    // wbkgd(nameRight, '4');
+    // wbkgd(sizeRight, '5');
+    // wbkgd(dateRight, '6');
+
+    mvwaddstr(nameWin, 1, 1, ".n");
+    mvwaddstr(nameWin, 1, sizeCol / 2 - 1, "Name");
+    mvwaddstr(sizeWin, 1, sizeCol / 2 - 1, "Size");
+    mvwaddstr(dateWin, 1, sizeCol / 2 - 1, "Date");
+    return win;
+}
 void initMc()
 {
     WINDOW* left;
@@ -45,46 +66,10 @@ void initMc()
 
     curs_set(0);
 
-    int sizeCol = COLS / 6;
-
-    left = newwin(LINES, COLS / 2, 0, 0);
-    WINDOW* nameLeft = subwin(left, LINES, sizeCol, 0, sizeCol * 0);
-    WINDOW* sizeLeft = subwin(left, LINES, sizeCol, 0, sizeCol * 1);
-    WINDOW* dateLeft = subwin(left, LINES, sizeCol, 0, sizeCol * 2);
-
-    box(left, 0, 0);
-    wborder(sizeLeft, ACS_VLINE, ACS_VLINE, ACS_HLINE, ACS_HLINE, ACS_TTEE, ACS_TTEE, ACS_BTEE, ACS_BTEE);
-    // wbkgd(nameLeft, '1');
-    // wbkgd(sizeLeft, '2');
-    // wbkgd(dateLeft, '3');
-
-    right = newwin(LINES, COLS / 2, 0, COLS / 2);
-    WINDOW* nameRight = subwin(right, LINES, sizeCol, 0, COLS / 2 + sizeCol * 0);
-    WINDOW* sizeRight = subwin(right, LINES, sizeCol, 0, COLS / 2 + sizeCol * 1);
-    WINDOW* dateRight = subwin(right, LINES, sizeCol, 0, COLS / 2 + sizeCol * 2);
-
-    box(right, 0, 0);
-    wborder(sizeRight, ACS_VLINE, ACS_VLINE, ACS_HLINE, ACS_HLINE, ACS_TTEE, ACS_TTEE, ACS_BTEE, ACS_BTEE);
-    // wbkgd(nameRight, '4');
-    // wbkgd(sizeRight, '5');
-    // wbkgd(dateRight, '6');
-
-    mvwaddstr(nameLeft, 1, 1, ".n");
-    mvwaddstr(nameLeft, 1, sizeCol / 2 - 1, "Name");
-    mvwaddstr(sizeLeft, 1, sizeCol / 2 - 1, "Size");
-    mvwaddstr(dateLeft, 1, sizeCol / 2 - 1, "Date");
-    mvwaddstr(nameRight, 1, 1, ".n");
-    mvwaddstr(nameRight, 1, sizeCol / 2 - 1, "Name");
-    mvwaddstr(sizeRight, 1, sizeCol / 2 - 1, "Size");
-    mvwaddstr(dateRight, 1, sizeCol / 2 - 1, "Date");
-
     start_color();
     init_pair(1, COLOR_WHITE, COLOR_BLUE);
     wattr_on(stdscr, COLOR_PAIR(1), NULL);
 
-    refresh();
-    wrefresh(left);
-    wrefresh(right);
 }
 int wprintDir()
 {
@@ -101,7 +86,12 @@ int wprintDir()
 int main()
 {
     initMc();
-    wprintDir();
+    WINDOW* right = createTable(0);
+    WINDOW* left = createTable(COLS/2);
+    refresh();
+    wrefresh(left);
+    wrefresh(right);
+//    wprintDir();
     int ch;
     bool exit = 0;
     while (!exit)
