e9b6d632910d8566d13d5e2b52828b4b293e671b
Major <n.boywalenko@yandex.ru>
2025-06-13 19:14:35 +0700

    finish first version task3

--- task10/secondTask.c
+++ task10/secondTask.c
@@ -10,11 +10,12 @@ int main()
   pid = fork();
   if(pid == 0)//первый ребенок rootа (1)
   {
-      printf("I am parent1, my pid=%d, my ppid=%d\n", getpid(),getppid());
+    printf("I am parent1, my pid=%d, my ppid=%d\n", getpid(),getppid());
     pid = fork();
     if(pid==0)//первый ребенок первого родителя(3)
     {
       printf("I am first child of parent1, my pid=%d, my ppid=%d\n", getpid(),getppid());
+      exit(3);
     }
     else if(pid > 0)
     {
@@ -22,10 +23,12 @@ int main()
       if(pid==0)//второй ребенок первого родителя(4)
       {
         printf("I am second child of parent1, my pid=%d, my ppid=%d\n", getpid(),getppid());
+        exit(4);
       }
-    wait(NULL); //4
+      wait(NULL); //4
     }
     wait(NULL); //3
+    exit(1);
   }
   else if(pid > 0)//root(0)
   {
@@ -38,11 +41,13 @@ int main()
       if(pid == 0)//ребенок второго родителя(5)
       {
         printf("I am child of parent2, my pid=%d, my ppid=%d\n", getpid(),getppid());
+        exit(5);
       }
-    wait(NULL); //5
+      wait(NULL); //5
+      exit(2);
     }
     wait(NULL); //2
   }
-    wait(NULL); //1
+  wait(NULL); //1
 }
 
--- task10/thirdTask.c
+++ task10/thirdTask.c
@@ -1,7 +1,50 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_LENGTH_NAME_PROGRAM 32
+#define MAX_LENGTH_FULL_PATH 48
+#define MAX_LENGTH_OPTIONS 32
 
 int main()
 {
+  char progName[MAX_LENGTH_NAME_PROGRAM];
+  char progOpt[MAX_LENGTH_OPTIONS];
+  char fullPathToApp[MAX_LENGTH_FULL_PATH];
+  pid_t pid;
+  
+  while(1)
+  {
+    printf("Enter the program name(enter \"exit\" to exit)\n");
+    scanf("%31s", progName);
+    printf("%s\n", progName);
+    if(strcmp(progName, "exit") == 0)
+    {
+      break;
+    }
+
+    printf("Enter the start options\n");
+    scanf("%31s", progOpt);
+
+    strncpy(fullPathToApp, "/usr/bin/", sizeof(fullPathToApp));
+    strcat(fullPathToApp, progName);
+
+    pid = fork();
+    if(pid == 0)
+    {
+      printf("I am child, my pid=%d, my ppid=%d\n", getpid(), getppid());
+    printf("%s\n", fullPathToApp);
+    printf("%s\n", progName);
+    printf("%s\n", progOpt);
+      if(execl(fullPathToApp,progName, progOpt, (char*)NULL)<0)
+      {
+        perror("Failed to execute program");
+      }
+      exit(0);
+    }
+    wait(NULL);
+  }
   return 0;
 }
 
