e6917ff744f841a71c89e90d3ab3b6bcd9aa1811
Major <n.boywalenko@yandex.ru>
2025-06-13 12:15:54 +0700

    Fixed task1

--- task10/main.c
+++ task10/main.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 
 int main()
 {
-  int ppid =0;
-  int pid =0;
+  pid_t pid =0;
+  int status;
+
   pid = fork();
   if(pid == 0)
   {
-    pid = getpid();
-    ppid = getppid();
-    printf("I am child, my pid=%d, my ppid=%d\n", pid,ppid);
+    printf("I am child, my pid=%d, my ppid=%d\n", getpid(),getppid());
+    exit(5);
   }
   else if(pid > 0)
   {
-    pid = getpid();
-    ppid = getppid();
-    printf("I am parent, my pid=%d, my ppid=%d\n", pid,ppid);
+    printf("I am parent, my pid=%d, my ppid=%d\n", getpid(),getppid());
+    wait(&status);
+    printf("status=%d\n", WEXITSTATUS(status));
   }
-  wait(NULL);
 }
