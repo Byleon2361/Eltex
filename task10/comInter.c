d0faeb9a7baeb2b72b904647bbe9f7c466ba864e
Major <n.boywalenko@yandex.ru>
2025-06-22 15:18:23 +0700

    First version pipline func

--- task10/comInter.c
+++ task10/comInter.c
@@ -13,13 +13,13 @@ int execInput(char *input)
   countComms = splitInputOnComms(input, comms);
   for(int i = 0; i < countComms; i++)
   {
-    /* printf("%s\n\n", comms[i]); */
+    printf("%s\n\n", comms[i]);
 
     argc = splitCommOnArgvs(comms[i], argv);
-    /* for(int j = 0; j < argc; j++) */
-    /* { */
-    /*   printf("%s\n", argv[j]); */
-    /* } */
+    for(int j = 0; j < argc; j++)
+    {
+      printf("%s\n", argv[j]);
+    }
     argc = 0;
   }
   if (strcmp(argv[0], "exit") == 0)
@@ -74,23 +74,58 @@ int splitCommOnArgvs(char* comm, char** argv)
     argv[argc] = (char*)NULL;
     return argc;
 }
-void execComm(char** argv)
+void execComm(char** argv, char** argv2)
 {
+    int fd[2];
     char fullPathToApp[MAX_LENGTH_FULL_PATH];
+    char fullPathToApp2[MAX_LENGTH_FULL_PATH];
+    char buf[MAX_BUFFER_SIZE];
 
     strncpy(fullPathToApp, "/usr/bin/", sizeof(fullPathToApp));
     strcat(fullPathToApp, argv[0]);
 
-    pid_t pid = fork();
-    if (pid == 0)
+    strncpy(fullPathToApp2, "/usr/bin/", sizeof(fullPathToApp2));
+    strcat(fullPathToApp2, argv2[0]);
+
+    if(pipe(fd) == -1)
+    {
+      perror("Failed pipe");
+      exit(EXIT_FAILURE);
+    }
+    
+    pid_t sender = fork();
+    pid_t reciever = fork();
+    if (sender == 0)
     {
         if (execv(fullPathToApp, argv) < 0)
         {
-            perror("Failed to execute program");
+          perror("Failed to execute program");
         }
-        exit(0);
+        
+    dup2(stdout, fd[1]);
+        close(fd[0]);
+        write(fd[1], buf, MAX_BUFFER_SIZE);
+        close(fd[1]);
+        exit(EXIT_SUCCESS);
+    }
+    else if(reciever == 0)
+    {
+      if (execv(fullPathToApp2, argv2) < 0)
+      {
+        perror("Failed to execute program");
+      }
+
+    dup2(stdin, fd[0]);
+      close(fd[1]);
+      read(fd[0], buf, MAX_BUFFER_SIZE);
+      close(fd[0]);
+      exit(EXIT_SUCCESS);
     }
     wait(NULL);
+    wait(NULL);
+
+    dup2(fd[1], stdout);
+    dup2(fd[0], stdin);
 }
 void freeComms(char** comms, int countComms)
 {
--- task10/comInter.h
+++ task10/comInter.h
@@ -6,16 +6,19 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <string.h>
+
 #define MAX_LENGTH_INPUT 128
 #define MAX_LENGTH_COMMAND 64
 #define MAX_LENGTH_WORD 32
 #define MAX_LENGTH_FULL_PATH 32
 #define MAX_COUNT_COMMS 8
 #define MAX_COUNT_ARGS 8
+#define MAX_BUFFER_SIZE 1024
+
 int execInput(char *input);
 int splitInputOnComms(char* input, char** argv);
 int splitCommOnArgvs(char* input, char** argv);
-void execComm(char** argv);
+void execComm(char** argv, char** argv2);
 void freeComms(char** comms, int countComms);
 void freeArgs(char** argv, int argc);
 
