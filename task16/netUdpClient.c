9b14e838c1e10a6b80cacd80b9c4a33695e20eb7
Major <n.boywalenko@yandex.ru>
2025-07-15 20:38:58 +0700

    Complete netUdpServer and netUdpClient

--- task16/netUdpClient.c
+++ task16/netUdpClient.c
@@ -1,5 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define MAX_LENGTH_MSG 20
 int main()
 {
+  struct sockaddr_in server, client;
+  char *sendMsg = "Hi";
+  char recvMsg[MAX_LENGTH_MSG];
+
+  int fd = socket(AF_INET, SOCK_DGRAM, 0);
+  if(fd == -1)
+  {
+    perror("Error create socket");
+    exit(EXIT_FAILURE);
+  }
+
+  struct in_addr ip;
+  inet_pton(AF_INET, "127.0.0.1", &ip);
+
+  client.sin_family = AF_INET;
+  client.sin_port = htons(8082);
+  client.sin_addr = ip;
+
+  if(bind(fd, (struct sockaddr *)&client, sizeof(struct sockaddr_in)) == -1)
+  {
+    close(fd);
+    perror("Error bind");
+    exit(EXIT_FAILURE);
+  }
+
+  server.sin_family = AF_INET;
+  server.sin_port = htons(8081);
+  server.sin_addr = ip;
+
+  if(connect(fd, (struct sockaddr*)&server, sizeof(server)) == -1)
+  {
+    close(fd);
+    perror("Error connect");
+    exit(EXIT_FAILURE);
+  }
+
+  send(fd, sendMsg, strlen(sendMsg)+1, 0);
+  recv(fd, recvMsg, MAX_LENGTH_MSG, 0);
+
+  printf("%s\n", recvMsg);
+
+  close(fd);
+
   return 0;
 }
--- task16/netUdpServer.c
+++ task16/netUdpServer.c
@@ -1,7 +1,51 @@
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define MAX_LENGTH_QUEUE_CLIENTS 1
+#define MAX_LENGTH_MSG 20
 int main()
 {
+  struct sockaddr_in server, client;
+  char *sendMsg = "Hello";
+  char recvMsg[MAX_LENGTH_MSG];
+
+  int fd = socket(AF_INET, SOCK_DGRAM, 0);
+  if(fd == -1)
+  {
+    perror("Error create socket");
+    exit(EXIT_FAILURE);
+  }
+
+  struct in_addr ip;
+  inet_pton(AF_INET, "127.0.0.1", &ip);
+
+
+  server.sin_family = AF_INET;
+  server.sin_port = htons(8081);
+  server.sin_addr = ip;
+
+  client.sin_family = AF_INET;
+  client.sin_port = htons(8082);
+  client.sin_addr = ip;
+
+  if(bind(fd, (struct sockaddr *)&server, sizeof(struct sockaddr_in)) == -1)
+  {
+    close(fd);
+    perror("Error bind");
+    exit(EXIT_FAILURE);
+  }
+
+  socklen_t clientLen = sizeof(client);
+  recvfrom(fd, recvMsg, MAX_LENGTH_MSG, 0, (struct sockaddr *)&client, &clientLen);
+  sendto(fd, sendMsg, strlen(sendMsg)+1, 0, (struct sockaddr *)&client, clientLen);
+
+  printf("%s\n", recvMsg);
+
+  close(fd);
+
   return 0;
 }
-
