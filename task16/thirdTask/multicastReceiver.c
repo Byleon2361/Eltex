665c47feb86f6b41731485ae2a1a4f38d9e159da
Major <n.boywalenko@yandex.ru>
2025-07-24 15:42:32 +0700

    Fixed multicastReceiver

--- task16/thirdTask/multicastReceiver.c
+++ task16/thirdTask/multicastReceiver.c
@@ -4,9 +4,23 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
+#include <net/if.h>
 #define MAX_LEN_MSG 32
-int main()
+#define MAX_LEN_ARG 12
+int main(int argc, char *argv[])
 {
+  char interface[MAX_LEN_ARG]; 
+  printf("argc %d\n", argc);
+  if(argc > 2)
+  {
+    fprintf(stderr, "Error: Too many args\n");
+    exit(EXIT_FAILURE);
+  }
+  else if (argc == 2) 
+  {
+    strncpy(interface, argv[1], MAX_LEN_ARG);
+  }
+
   struct sockaddr_in receiver;
   char msg[MAX_LEN_MSG];
   int fd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -19,6 +33,7 @@ int main()
   struct in_addr ipLocalAddr;
   inet_pton(AF_INET, "224.0.0.1", &ipGroupAddr);
   inet_pton(AF_INET, "0.0.0.0", &ipLocalAddr);
+
   memset(&receiver, 0, sizeof(receiver));
   receiver.sin_family = AF_INET;
   receiver.sin_addr = ipLocalAddr;
@@ -27,6 +42,7 @@ int main()
   struct ip_mreqn mreqn;
   mreqn.imr_multiaddr = ipGroupAddr;
   mreqn.imr_address = ipLocalAddr;
+  mreqn.imr_ifindex = (argc == 2) ? if_nametoindex(interface) : 0;
 
   if(setsockopt(fd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreqn, sizeof(mreqn)) == -1)
   {
